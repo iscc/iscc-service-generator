1dd41fcef4ae5164836d8df54ca0cac1e7a2382c
Titusz Pan <titusz.pan@gmail.com>
2022-02-02 19:21:36 +0100

    Add .scratch directory for development

--- iscc_service_generator/settings.py
+++ iscc_service_generator/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = "iscc_service_generator.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / ".scratch/db.sqlite3",
     }
 }
 
