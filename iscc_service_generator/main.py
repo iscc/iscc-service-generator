6c883848f9032b1fff9238d2db839749782e386d
Titusz Pan <titusz.pan@gmail.com>
2022-02-10 13:25:25 +0100

    Fix runserver in main.py

--- iscc_service_generator/main.py
+++ iscc_service_generator/main.py
@@ -3,7 +3,10 @@ import uvicorn
 
 def run_server():
     uvicorn.run(
-        "iscc_service_generator.main:app", host="127.0.0.1", port=8000, reload=False
+        "iscc_service_generator.asgi:application",
+        host="127.0.0.1",
+        port=8000,
+        reload=True,
     )
 
 
