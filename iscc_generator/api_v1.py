b3b90b787d00dd936fa64084cf6eb382f23ca84c
Titusz Pan <titusz.pan@gmail.com>
2022-02-10 13:40:40 +0100

    Refactor endpoints to async code.

--- iscc_generator/api_v1.py
+++ iscc_generator/api_v1.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
 from typing import Optional
+from asgiref.sync import sync_to_async
 from django_q.tasks import async_task, result
 from django_q.models import Task, OrmQ
 from ninja import Router, UploadedFile, File, Form, ModelSchema, Schema, Field
@@ -19,7 +20,7 @@ class Message(Schema):
 class IsccRequest(ModelSchema):
     class Config:
         model = IsccCode
-        model_fields = ["name", "description"]
+        model_fields = ["source_url", "name", "description"]
 
 
 class IsccResponse(ModelSchema):
@@ -47,12 +48,12 @@ class TaskResponse(Schema):
 
 @router.post(
     "/iscc_code",
-    response={200: IsccResponse, 202: TaskResponse, 503: Message},
+    response={200: IsccResponse, 202: TaskResponse, 400: Message, 503: Message},
     exclude_defaults=True,
     summary="Generate ISCC-CODE",
     tags=["iscc_code"],
 )
-def generate_iscc_code(
+async def generate_iscc_code(
     request,
     source_file: Optional[UploadedFile] = File(None),
     meta: IsccRequest = Form(...),
@@ -60,17 +61,22 @@ def generate_iscc_code(
     """
     ## Generate an ISCC for a media asset.
 
-    Provide at least a `source_file` or optionally a `source_url`. If processing finishes fast
+    Provide at least a `source_file` or optionally a `source_url`. If processing finishes in time
     you will receive an `IsccResponse`. If processing exeeds the time limit you will receive
-    a `TaskResponse` instead to poll for the result at /task/{task_id}.
+    a `TaskResponse` to poll for the result at /task/{task_id}.
     """
-    ico = IsccCode.objects.create(source_file=source_file, **meta.dict())
-    task_id = async_task(create_iscc_code, ico.id)
-    task_result = result(task_id, config.PROCESSING_TIMEOUT)
+
+    if not source_file and not meta.source_url:
+        return 400, Message(message="either source_file or source_url is required")
+
+    ico = await async_create_iscc_code(source_file, meta)
+    task_id = await async_create_task(ico.pk)
+    task_result = await async_wait_for_task(task_id)
     if task_result:
-        return 200, IsccCode.objects.get(id=ico.id)
+        obj = await async_get_iscc_code(ico.pk)
+        return 200, obj
     else:
-        task = find_task(task_id)
+        task = await async_find_task(task_id)
         if not task:
             return 503, Message(message="Task not found")
         return 202, task
@@ -82,14 +88,38 @@ def generate_iscc_code(
     exclude_none=True,
     tags=["iscc_code"],
 )
-def get_task(request, task_id: str):
-    task = find_task(task_id)
+async def get_task(request, task_id: str):
+    task = await async_find_task(task_id)
     if not task:
         return 404, Message(message="Task not found")
     return 200, task
 
 
-def find_task(task_id):
+####################################################################################################
+# Sync to Async functions                                                                          #
+####################################################################################################
+
+
+@sync_to_async
+def async_create_iscc_code(source_file, meta):
+    if source_file:
+        return IsccCode.objects.create(source_file=source_file, **meta.dict())
+    else:
+        return IsccCode.objects.create(**meta.dict())
+
+
+@sync_to_async
+def async_create_task(iscc_pk) -> str:
+    return async_task(create_iscc_code, iscc_pk)
+
+
+@sync_to_async
+def async_wait_for_task(task_id):
+    return result(task_id, config.PROCESSING_TIMEOUT * 1000)
+
+
+@sync_to_async
+def async_find_task(task_id):
     task = None
     try:
         task = Task.objects.get(id=task_id)
@@ -99,3 +129,8 @@ def find_task(task_id):
                 task = queued.task()
                 break
     return task
+
+
+@sync_to_async
+def async_get_iscc_code(pk):
+    return IsccCode.objects.get(pk=pk)
