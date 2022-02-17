3d84e6a74da45575d69ec0142bbb9d2f815345d2
Titusz Pan <titusz.pan@gmail.com>
2022-02-17 10:55:38 +0100

    Use package version for NinjaAPI

--- iscc_service_generator/api_v1.py
+++ iscc_service_generator/api_v1.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 from ninja import NinjaAPI
 from iscc_generator.api_v1 import router as generator_router
+from iscc_service_generator import __version__
+
 
 api = NinjaAPI(
     title="ISCC Generator",
-    version="1.0.0",
+    version=__version__,
     description="A webservice for generating ISCC codes",
 )
 api.add_router("/", generator_router, tags=["generate"])
