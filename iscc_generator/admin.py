819a0e423538e9336a6d18d788f1bceeaf1289f8
Titusz Pan <titusz.pan@gmail.com>
2022-02-17 10:54:38 +0100

    Cleanup dashboard forms and generate ISCC on form submit

--- iscc_generator/admin.py
+++ iscc_generator/admin.py
@@ -1,13 +1,13 @@
 from typing import Optional
 
 from django.contrib import admin
+from django import forms
 from django.db import models
 from django_json_widget.widgets import JSONEditorWidget
 from django_object_actions import DjangoObjectActions, takes_instance_or_queryset
 from django_q.tasks import async_task
-
 from iscc_generator.models import IsccCode
-from iscc_generator.tasks import create_iscc_code, download_file
+from iscc_generator.tasks import create_iscc_code
 
 
 @admin.register(IsccCode)
@@ -24,7 +24,7 @@ class IsccCodeAdmin(DjangoObjectActions, admin.ModelAdmin):
 
     list_display = [
         "id",
-        "iscc",
+        "iscc_monospaced",
         "source_file_name",
         "name",
         "source_file_mediatype",
@@ -36,20 +36,39 @@ class IsccCodeAdmin(DjangoObjectActions, admin.ModelAdmin):
     ]
 
     fields = [
+        "iscc",
         "source_file",
         "source_url",
         "source_file_mediatype",
         "source_file_size",
         "name",
         "description",
+        "metadata",
         "result",
     ]
 
-    readonly_fields = ["iscc", "source_file_mediatype", "source_file_size"]
+    readonly_fields = [
+        "iscc",
+        "source_file_mediatype",
+        "source_file_size",
+        "source_file_name",
+    ]
 
     formfield_overrides = {
-        models.JSONField: {"widget": JSONEditorWidget},
+        models.URLField: {"widget": forms.URLInput(attrs={"size": 98})},
+        models.CharField: {"widget": forms.TextInput(attrs={"size": 98})},
+        models.TextField: {"widget": forms.Textarea(attrs={"cols": 98, "rows": 8})},
+        models.JSONField: {"widget": JSONEditorWidget(width="53em", height="28em")},
     }
+
+    change_actions = ["action_create_iscc"]
+    actions = ["action_create_iscc"]
+    actions = ["action_create_iscc"]
+
+    def save_model(self, request, obj, form, change):
+        """Create ISCC on Save"""
+        super().save_model(request, obj, form, change)
+        create_iscc_code(obj.pk)
 
     def get_readonly_fields(self, request, obj: Optional[IsccCode] = None):
         if obj and obj.source_file:
@@ -61,9 +80,5 @@ class IsccCodeAdmin(DjangoObjectActions, admin.ModelAdmin):
         for obj in queryset:
             async_task(create_iscc_code, obj.pk)
 
-    action_create_iscc.label = "Regenerate ISCC"  # optional
-    action_create_iscc.short_description = "Regenerate selected ISCC Codes"
-
-    change_actions = ["action_create_iscc"]
-    # changelist_actions = ["action_create_iscc"]
-    actions = ["action_create_iscc"]
+    action_create_iscc.label = "Generate ISCC"  # optional
+    action_create_iscc.short_description = "Generate ISCC Codes for selected entries"
