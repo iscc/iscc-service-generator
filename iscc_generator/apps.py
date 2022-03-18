c3004e4e537c792147ba49ec19a3c16f59a78af7
Titusz Pan <titusz.pan@gmail.com>
2022-03-18 21:32:14 +0100

    Don´t set global BigAutoField.

--- iscc_generator/apps.py
+++ iscc_generator/apps.py
@@ -2,6 +2,5 @@ from django.apps import AppConfig
 
 
 class IsccGeneratorConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
     name = "iscc_generator"
     verbose_name = "ISCC Generator"
